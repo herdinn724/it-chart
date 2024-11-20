f73d73e326cc96d4d2a0ccfb3ec62db7b49544e6
herdinn724 <bumaberau3@gmail.com>
2024-11-20 09:07:42 +0800

    asset plugin

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -13,15 +13,15 @@
     <link rel="stylesheet" href="{{asset('assets_pluginAdmin/vendors/jquery-bar-rating/css-starts.css') }}">
     <!-- endinject -->
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="../assets/vendors/jquery-bar-rating/css-stars.css" />
-    <link rel="stylesheet" href="../assets/vendors/font-awesome/css/font-awesome.min.css" />
+    
+    <link rel="stylesheet" href="{{asset('assets_pluginAdmin/vendors/font-awesome/css/font-awesome.min.css') }}" />
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <!-- endinject -->
     <!-- Layout styles -->
-    <link rel="stylesheet" href="../assets/css/demo_1/style.css" />
+    <link rel="stylesheet" href="{{asset('assets_pluginAdmin/css/demo_1/style.css') }}" />
     <!-- End layout styles -->
-    <link rel="shortcut icon" href="../assets/images/favicon.png" />
+    <link rel="shortcut icon" href="{{asset('assets_pluginAdmin/assets/images/favicon.png') }}" />
   </head>
   <body>
     <div class="container-scroller">
@@ -869,26 +869,27 @@
     </div>
     <!-- container-scroller -->
     <!-- plugins:js -->
-    <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
+    <script src="{{asset('assets_pluginAdmin/vendors/js/vendor.bundle.base.js')}}"></script>
     <!-- endinject -->
     <!-- Plugin js for this page -->
-    <script src="../assets/vendors/jquery-bar-rating/jquery.barrating.min.js"></script>
-    <script src="../assets/vendors/chart.js/Chart.min.js"></script>
-    <script src="../assets/vendors/flot/jquery.flot.js"></script>
-    <script src="../assets/vendors/flot/jquery.flot.resize.js"></script>
-    <script src="../assets/vendors/flot/jquery.flot.categories.js"></script>
-    <script src="../assets/vendors/flot/jquery.flot.fillbetween.js"></script>
-    <script src="../assets/vendors/flot/jquery.flot.stack.js"></script>
+    <script src="{{asset('assets_pluginAdmin/vendors/jquery-bar-rating/jquery.barrating.min.js')}}"></script>
+    <script src="{{asset('assets_pluginAdmin/vendors/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('assets_pluginAdmin/vendors/flot/jquery.flot.js')}}"></script>
+    <script src="{{asset('assets_pluginAdmin/vendors/flot/jquery.flot.resize.js')}}"></script>
+    <script src="{{asset('assets_pluginAdmin/vendors/flot/jquery.flot.categories.js')}}"></script>
+    <script src="{{asset('assets_pluginAdmin/vendors/flot/jquery.flot.fillbetween.js')}}"></script>
+    <script src="{{asset('assets_pluginAdmin/vendors/flot/jquery.flot.stack.js')}}"></script>
     <!-- End plugin js for this page -->
+    {{asset('assets_pluginAdmin')}}
     <!-- inject:js -->
-    <script src="../assets/js/off-canvas.js"></script>
-    <script src="../assets/js/hoverable-collapse.js"></script>
-    <script src="../assets/js/misc.js"></script>
-    <script src="../assets/js/settings.js"></script>
-    <script src="../assets/js/todolist.js"></script>
+    <script src="{{asset('assets_pluginAdmin/js/off-canvas.js')}}"></script>
+    <script src="{{asset('assets_pluginAdmin/js/hoverable-collapse.js')}"></script>
+    <script src="{{asset('assets_pluginAdmin/js/misc.js')}}"></script>
+    <script src="{{asset('assets_pluginAdmin/js/settings.js')}}"></script>
+    <script src="{{asset('assets_pluginAdmin/js/todolist.js')}}"></script>
     <!-- endinject -->
     <!-- Custom js for this page -->
-    <script src="../assets/js/dashboard.js"></script>
+    <script src="{{asset('assets_pluginAdmin/js/dashboard.js')}}"></script>
     <!-- End custom js for this page -->
   </body>
 </html>
