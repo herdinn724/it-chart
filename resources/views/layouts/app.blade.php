752288e2a25eaa6f81709fb9fe95057f72dd2c12
herdinn724 <bumaberau3@gmail.com>
2024-11-25 13:57:25 +0800

    blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,9 +36,29 @@
         <!-- partial:partials/_navbar.html -->
         @include('layouts.partial.navbar')
         <!-- partial -->
-        @include('layouts.dashboard')
+        <div class="main-panel">
+          <div class="content-wrapper pb-0">
+            <div class="page-header flex-wrap">
+              <div class="header-left">
+                <button class="btn btn-primary mb-2 mb-md-0 mr-2"> Create new document </button>
+                <button class="btn btn-outline-primary bg-white mb-2 mb-md-0"> Import documents </button>
+              </div>
+              <div class="header-right d-flex flex-wrap mt-2 mt-sm-0">
+                <div class="d-flex align-items-center">
+                  <a href="#">
+                    <p class="m-0 pr-3">Dashboard</p>
+                  </a>
+                  <a class="pl-3 mr-4" href="#">
+                    <p class="m-0">ADE-00234</p>
+                  </a>
+                </div>
+                <button type="button" class="btn btn-primary mt-2 mt-sm-0 btn-icon-text">
+                  <i class="mdi mdi-plus-circle"></i> Add Prodcut </button>
+              </div>
+            </div>
         <!-- main-panel ends -->
       </div>
+      
       <!-- page-body-wrapper ends -->
     </div>
     <!-- container-scroller -->
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,23 +1,4 @@
-<div class="main-panel">
-          <div class="content-wrapper pb-0">
-            <div class="page-header flex-wrap">
-              <div class="header-left">
-                <button class="btn btn-primary mb-2 mb-md-0 mr-2"> Create new document </button>
-                <button class="btn btn-outline-primary bg-white mb-2 mb-md-0"> Import documents </button>
-              </div>
-              <div class="header-right d-flex flex-wrap mt-2 mt-sm-0">
-                <div class="d-flex align-items-center">
-                  <a href="#">
-                    <p class="m-0 pr-3">Dashboard</p>
-                  </a>
-                  <a class="pl-3 mr-4" href="#">
-                    <p class="m-0">ADE-00234</p>
-                  </a>
-                </div>
-                <button type="button" class="btn btn-primary mt-2 mt-sm-0 btn-icon-text">
-                  <i class="mdi mdi-plus-circle"></i> Add Prodcut </button>
-              </div>
-            </div>
+
             <!-- first row starts here -->
             <div class="row">
               <div class="col-xl-9 stretch-card grid-margin">
